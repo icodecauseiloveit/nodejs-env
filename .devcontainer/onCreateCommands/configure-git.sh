30ebf285fdcabe6886f19e688e0ba1fd52adb8eb
Gary Baldrich <gbaldrich@gmail.com>
2023-02-06 00:18:36 +0000

    Set the deafult branch main in stead of master

--- .devcontainer/onCreateCommands/configure-git.sh
+++ .devcontainer/onCreateCommands/configure-git.sh
@@ -3,5 +3,7 @@
 # Setting up Git
 git config --global user.name "$NAME"
 git config --global user.email $EMAIL
+git config --global init.defaultBranch main
 git config --global core.editor "code --wait"
+
 echo "Git configured successfully!"
